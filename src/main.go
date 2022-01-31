c359465ee2ec45bc2fbc2358d78f1760c974bc8e
Kon Aoki <kon.aoki.us@gmail.com>
2022-01-31 10:09:19 -0800

    added flags

--- src/main.go
+++ src/main.go
@@ -1,7 +1,23 @@
 package main
 
 import "fmt"
+import "flag"
 
 func main() {
-  fmt.Println("hello world")
+
+  addPtr := flag.String("add", "", "Todo to add")
+  removePtr := flag.Int("remove", 0, "Todo ID to remove")
+
+  flag.Parse()
+
+  if flag.NFlag() == 0 {
+    flag.PrintDefaults()
+  }
+
+  fmt.Println("add:", *addPtr)
+  fmt.Println("remove:", *removePtr)
+
+  if *addPtr != "" {
+
+  }
 }
