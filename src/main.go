3980e3caaee62315d3bf9246a0d31f7dcde9a00f
Kon Aoki <kon.aoki.us@gmail.com>
2022-01-31 11:18:05 -0800

    added sqlite setup

--- src/main.go
+++ src/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
-import "flag"
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+
+	createDatabase()
+
+	db, err := sql.Open("sqlite3", "./todo.db")
+	checkErr(err)
+	createStmt, err := db.Prepare(`CREATE TABLE todo(
+    "id" INT,
+    "task" VARCHAR(255),
+    "done" BOOL
+  )`)
+	createStmt.Exec()
+	checkErr(err)
+	statement, err := db.Prepare("INSERT INTO todo(id, task, done) values(?,?,?)")
+	checkErr(err)
+	statement.Exec(1, "test task", false)
+
+	// query
+	rows, err := db.Query("SELECT * FROM todo")
+	checkErr(err)
+	var id int
+	var task string
+	var done bool
+
+	for rows.Next() {
+		err = rows.Scan(&id, &task, &done)
+		checkErr(err)
+		fmt.Println(id)
+		fmt.Println(task)
+		fmt.Println(done)
+	}
+
+	rows.Close()
 
 	addPtr := flag.String("add", "", "Todo to add")
 	removePtr := flag.Int("remove", 0, "Todo ID to remove")
@@ -24,4 +63,18 @@ func main() {
 
 func addTodo(todo string) {
 
+}
+
+func createDatabase() {
+	file, err := os.Create("todo.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	file.Close()
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
